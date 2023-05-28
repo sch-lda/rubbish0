e21f18a9696272cac5d47a90eb42b263249a5124
HCR-750F <54973190+sch-lda@users.noreply.github.com>
2023-05-28 19:57:36 +0800

    LSTexpress sh

--- LSTexpress/lstexpress.sh
+++ LSTexpress/lstexpress.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-url="https://hqshi.cn/api/post?from=Miniprogram&user=MzXk&nickname=7660&platform=default&expire=900"
+read -p "请输入查询者ID(可直接回车): " user
+user=${user:-MzXk} 
+read -p "请输入查询目标SC名称(必填,不区分大小写): " nickname
+nickname=${nickname:-shuaige66} 
+read -p "请输入循环查询间隔(s)可不填: " sleep_time
+sleep_time=${sleep_time:-930} 
+
+url="https://hqshi.cn/api/post?from=Miniprogram&user=$user&nickname=$nickname&platform=default&expire=900"
 
 while true; do
     curl -X GET "$url"
-    sleep 930
-done
+    sleep $sleep_time
+done 
