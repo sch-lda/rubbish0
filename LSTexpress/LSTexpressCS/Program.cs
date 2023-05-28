cb9cea28c9f66beb66f5580b8e4806094e6552c3
HCR-750F <54973190+sch-lda@users.noreply.github.com>
2023-05-28 19:02:09 +0800

    LSTexpressCS upd1

--- LSTexpress/LSTexpressCS/Program.cs
+++ LSTexpress/LSTexpressCS/Program.cs
@@ -25,7 +25,12 @@ catch (Exception ex)
 
     static void Main(string[] args)
     {
-        string url = "https://hqshi.cn/api/post?from=Miniprogram&user=MzXk&nickname=xxxxxxx&platform=default&expire=900";
+        string user1, user2;
+        Console.WriteLine("请输入查询者ID(可瞎填)：");
+        user1 = Console.ReadLine();
+        Console.WriteLine("请输入目标SC用户名(不区分大小写)：");
+        user2 = Console.ReadLine();
+        string url = "https://hqshi.cn/api/post?from=Miniprogram&user="+ user1 +"MzXk&nickname="+user2+"&platform=default&expire=900";
         Timer timer = new Timer((state) => SendRequest(url), null, TimeSpan.Zero, TimeSpan.FromSeconds(930));
         Console.WriteLine(DateAndTime.Now + "开始运行");
         Console.ReadLine();
