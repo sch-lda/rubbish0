2b40174639ec6e659047e5747913b53524d34734
ch s <SongCH@DESKTOP-EFD5TVV>
2023-10-12 10:19:38 +0800

    0

---
+++ strout/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string luaFileName = "C:/Users/SongCH/AppData/Roaming/YimMenu/scripts/sch.lua"; // 替换为你的Lua文件路径
+        string outputFileName = "C:/Users/SongCH/AppData/Roaming/YimMenu/scripts/sch.lua.txt"; // 替换为输出文件名
+
+        try
+        {
+            string luaCode = File.ReadAllText(luaFileName);
+            string[] keywords = { "log", "button", "gui", "checkbox" };
+            string pattern = "\"(.*?)\""; // 匹配双引号内的内容
+
+            MatchCollection matches = Regex.Matches(luaCode, pattern);
+            using (StreamWriter writer = new StreamWriter(outputFileName))
+            {
+                foreach (Match match in matches)
+                {
+                    string line = GetLineContainingMatch(luaCode, match);
+                    if (ContainsAnyKeyword(line, keywords) && ContainsChineseCharacters(match.Groups[1].Value) && !IsWhiteSpaceOrPercent(match.Groups[1].Value))
+                    {
+                        writer.WriteLine(match.Groups[1].Value);
+                    }
+                }
+            }
+
+            Console.WriteLine("提取完成，字符串已导出到 " + outputFileName);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("发生错误: " + ex.Message);
+        }
+    }
+
+    static string GetLineContainingMatch(string text, Match match)
+    {
+        int index = text.IndexOf(match.Value);
+        if (index >= 0)
+        {
+            int lineStart = text.LastIndexOf('\n', index) + 1;
+            int lineEnd = text.IndexOf('\n', index);
+            if (lineEnd < 0)
+            {
+                lineEnd = text.Length;
+            }
+            return text.Substring(lineStart, lineEnd - lineStart);
+        }
+        return string.Empty;
+    }
+
+    static bool ContainsAnyKeyword(string line, string[] keywords)
+    {
+        foreach (string keyword in keywords)
+        {
+            if (line.Contains(keyword))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static bool ContainsChineseCharacters(string value)
+    {
+        return Regex.IsMatch(value, @"[\u4e00-\u9fa5]");
+    }
+
+    static bool IsWhiteSpaceOrPercent(string value)
+    {
+        return string.IsNullOrWhiteSpace(value) || value.Trim() == "%";
+    }
+}
---
+++ strout/strrepl/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        // 读取in.txt和tar.txt的内容
+        string[] inLines = File.ReadAllLines("D:\\zh.txt");
+        string[] tarLines = File.ReadAllLines("D:\\en.txt");
+
+        if (inLines.Length != tarLines.Length)
+        {
+            Console.WriteLine("in.txt和tar.txt行数不匹配。");
+            return;
+        }
+
+        // 读取Lua源文件内容
+        string luaSource = File.ReadAllText("D:\\sch.lua");
+
+        // 使用正则表达式查找双引号内的字符串
+        string result = Regex.Replace(luaSource, "\"(.*?)\"", match =>
+        {
+            string contentInsideQuotes = match.Groups[1].Value;
+            int index = Array.IndexOf(inLines, contentInsideQuotes);
+
+            if (index != -1)
+            {
+                return "\"" + tarLines[index] + "\"";
+            }
+            else
+            {
+                return match.Value; // 未找到匹配，保留原始字符串
+            }
+        });
+
+        // 写入替换后的Lua源文件
+        File.WriteAllText("D:\\ensch.lua", result);
+
+        Console.WriteLine("替换完成。");
+    }
+}
